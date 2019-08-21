f0d3f81352314cd444c2a675c8a85c1a741443c6
wx347332690 <347332690@qq.com>
2019-08-21 09:38:35 +0800

    456

---
+++ src/main/scala/utils/Utils2Type.scala
@@ -0,0 +1,21 @@
+package utils
+
+object Utils2Type {
+  def toDouble(str: String): Double = {
+    try {
+      str.toDouble
+    } catch {
+      case _:Exception => 0.0
+    }
+  }
+
+  def toInt(str: String): Int = {
+    try (
+      str.toInt
+      ) catch {
+      case _:Exception => 0
+    }
+  }
+
+}
+
